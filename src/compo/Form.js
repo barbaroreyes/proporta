9464df991014ec7cb2756278a4decd291f8ed2eb
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-13 12:00:01 -0500

    fixed-image

---
+++ src/compo/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form >
+        <input type='text' placeholder='name' value= 'name'/>
+        <input type='email' placeholder='email' value= 'email'/>
+        <input type='submit' />
+      
+    </form>
+  );
+}
+
+export default Form;
