43e8046ba185bc96f5def62efb07c66d8c16771c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-12 22:30:26 -0500

    added-home

--- src/compo/Footer.js
+++ src/compo/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      footer
+      <Link  to='/'>Home</Link>
+      <Link to='/portafolio'>Portafolio</Link>
+      <Link to='/contact'>Contact</Link>
     </footer>
   );
 }
