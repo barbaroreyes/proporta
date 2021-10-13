48ad23e74d7453c83e969b053ccd62ba5f1c9df1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-12 23:36:01 -0500

    added some product styles

--- src/compo/Project.js
+++ src/compo/Project.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 const Project = (props) => {
-    console.log('props in project',props.name)
-  return (
-    <div>
-      {props.name}
-    </div>
-  );
+    const [isFlipped,setIsFlipped] = useState(false)
+    const handleClick = () => {
+        setIsFlipped(!isFlipped)
+    }
+    return (
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+          <div className="front"
+        //   onClick={handleClick}
+          >
+            {props.name}
+            <a href ={props.github}>Git</a>
+            <a href ={props.app}>App</a>
+            
+          </div>
+  
+          <div div className="back"
+          onClick={handleClick}>
+            {props.skills}
+            
+
+          </div>
+        </ReactCardFlip>
+      )
 }
 
 export default Project;
