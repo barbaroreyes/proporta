cc3a43225cf5cd49d88d3b844e2aa7417d896b4b
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-13 02:02:44 -0500

    adde-picture

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Picture from '../info/fotoperfil.jpeg'
 const Header = () => {
   return (
     <header>
       Barbaro Reyes
+      <img  src ={Picture}  alt ='phot'/>
     </header>
   );
 }
--- src/compo/Project.js
+++ src/compo/Project.js
@@ -4,6 +4,7 @@ const Project = (props) => {
     return (
         <div className="front">
             {props.name}
+            {props.skills}
             <a href ={props.github}>Git</a>
             <a href ={props.app}>App</a>
             </div>
